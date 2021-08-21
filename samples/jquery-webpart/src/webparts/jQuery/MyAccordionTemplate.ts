8900c4221cdd6b5e18e56719bad1f4b338c874d6
Viral Naik <84363913+ViralBDONZ@users.noreply.github.com>
2021-08-21 13:27:08 +1200

    updated ts

--- samples/jquery-webpart/src/webparts/jQuery/MyAccordionTemplate.ts
+++ samples/jquery-webpart/src/webparts/jQuery/MyAccordionTemplate.ts
@@ -4,17 +4,18 @@ export default class MyAccordionTemplate {
         <h3>FOREWORD</h3>
         <div>
             <p>
-The purpose of this playbook is to provide BDO Independent Member Firms with some of the tools and insights they need to support the roll out of virtual CFO services to new and existing clients, with the idea being that sharing the resources already created we leverage efficiencies and provide a comprehensive market offering. 
-The provision of virtual Chief Financial Officer services is seen to be a growth opportunity for BDO in NZ.  BDO is uniquely placed to offer such services to mid market organisations that require CFO level services due to our position in that market and lower cost structure compared to Big 4 competitors.   
-            </p>
+            Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+            Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+            ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+            lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.</p>
         </div>
         <h3>What are vCFO services? </h3>
         <div>
             <p>
-            Virtual CFO is effectively outsourcing the finance function.  It can involve BDO acting as interim CFO and can have a senior person onsite to handle audit/IFRS reporting, payroll, bookkeeping and other finance team functions.   
-Providing strategic and commercial advice to an organisation based around management reporting, KPIs, actual v budget, cashflow advice & analysis along with process improvement, strategy development & execution.  At a base level we are providing assurance that regular reporting and compliance is smooth, efficient, and reliable.  At a high level we are providing support to the CEO and Board particularly for key decision making and related financial impacts. 
-The key is that the services provided by BDO are scalable depending on the size and needs of the client.  For example, for smaller clients a quarterly meeting with the owner may be adequate vs a larger client where BDO is engaged to attend monthly Board meetings and contribute to weekly management catchups.   
-<img src="/src/images/diagram01.png" alt="diagram1">
+            Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+            Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+            ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+            lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
 </p>
         </div>
         <h3>Section 3</h3>
