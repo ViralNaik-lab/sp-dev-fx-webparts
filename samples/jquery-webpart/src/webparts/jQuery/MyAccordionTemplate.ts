b513fb383c2c6b59e9e5005709f5723d817b07f2
Viral Naik <84363913+ViralBDONZ@users.noreply.github.com>
2021-08-21 13:33:38 +1200

    updated html

--- samples/jquery-webpart/src/webparts/jQuery/MyAccordionTemplate.ts
+++ samples/jquery-webpart/src/webparts/jQuery/MyAccordionTemplate.ts
@@ -4,7 +4,7 @@ export default class MyAccordionTemplate {
         <h3>FOREWORD</h3>
         <div>
             <p>
-            Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+            Naam   enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
             Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
             ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
             lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.</p>
